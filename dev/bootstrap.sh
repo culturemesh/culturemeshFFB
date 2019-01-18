b9b5326eee2894580f85558f8aab44d5acbd5b9c
cs <cs.temporary@icloud.com>
2019-01-18 03:04:08 +0000

    Add setup script to configure hooks

--- dev/bootstrap.sh
+++ dev/bootstrap.sh
@@ -1,1 +1,21 @@
 #!/usr/bin/env bash
+
+set -e
+
+install_hooks() {
+    rm -rf ./.git/hooks
+    cp -r ./dev/hooks ./.git/hooks
+}
+
+log() {
+    echo "$1"
+}
+
+if [ ! -d ".git" ]; then {
+    log "Aborting because you are not at the root of a git repository"
+    exit 1
+}
+fi
+
+log "Configuring git hooks"
+install_hooks
